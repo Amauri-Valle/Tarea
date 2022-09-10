2a995e18faad292e74705b102b5aaf56cd985b9f
DESKTOP-4J1BGEL\Dell Inspiron <amaurisamirvalle@gmail.com>
2022-09-10 14:57:46 -0600

    Develop 2

--- Trabajo 9/Program.cs
+++ Trabajo 9/Program.cs
@@ -6,13 +6,19 @@ namespace Trabajo_9
     {
         static void Main(string[] args)
         {
-            int num1, num2;
+            int num1, num2, resultado;
+            string Palabra="Chinga a tu madre";
 
             Console.WriteLine("Escriba el primer numero");
             num1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Escriba el segundo numero");
             num2 = int.Parse(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            Console.WriteLine("El resultado es: " + resultado+ "" + Palabra);
+            
         }
     }
 }
